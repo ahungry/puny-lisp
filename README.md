8e12eca33f6d67d2537964c16b3f2c9466b150da
Matthew Carter <m@ahungry.com>
2015-02-04 02:13:41 -0500

    Add license and README

---
+++ README.md
@@ -0,0 +1,10 @@
+Puny Lisp
+===============
+
+The weakest lisp around!
+
+To compile, simply run 'make' in the repository.
+
+## Goal(s)
+To create a tiny lisp which is capable of compiling down
+to javascript via emscripten.
--- puny.c
+++ puny.c
@@ -1,3 +1,21 @@
+/*
+ * Puny Lisp - The weakest lisp around!
+ * Copyright (C) 2015 Matthew Carter <m@ahungry.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -45,7 +63,7 @@ int fn_basic_math (char op, int a, int b)
     }
 }
 
-/**
+/*
  * Seek until we hit an innermost pair of parens
  */
 void parse_innermost (char* line)
@@ -218,7 +236,7 @@ int parse_buffer (char* line)
   return 0; // A general success in parsing.
 }
 
-/**
+/*
  * Start
  */
 int main (int argc, char *argv[])
